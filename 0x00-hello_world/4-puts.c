9a2dc1cc846c6cad55394fb50ddcb2eb1aec650d
root <greendawninitiative@gmail.com>
2021-02-06 15:11:23 +0000

    A properly written C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+*main - main code block
+*Descrition sends the string to the stdout
+*Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
