2d080bd038f4f97658e2cb86bed9965117316512
root <greendawninitiative@gmail.com>
2021-02-06 15:16:59 +0000

    Another C program using print f function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - main code block
+*Descrition sends the string to the stdout
+*Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, the outcome is a proper piece of art/n");
+return (0);
+}
