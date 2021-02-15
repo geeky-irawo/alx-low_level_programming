a036048d27815cfc80a795b2a84efdd709b978e4
root <greendawninitiative@gmail.com>
2021-02-15 23:39:17 +0000

    This is my commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,20 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0){
-printf("%i is positive", n);
+
+if
+(n>0)
+{
+printf("%i is positive\n", n);
 }
-else if(n == 0){
-printf("%i is zero", n);
+else
+(n == 0)
+{
+printf("%i is zero\n", n);
 }
-else ( n < 0);{
-printf("%i is negative", n);
+else
+{
+printf("%i is negative\n", n);
 }
 return (0);
 }
